4313014c942babda91b967c22f5b2846e5a40e6a
Eason <a28165965@gmail.com>
2022-05-31 17:10:50 +0800

    增加 prop-type,eslint,prettier 套件

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-import 'bootstrap';
+// import 'bootstrap';
 import Navbar from './components/NavBar';
 import Footer from './components/Footer';
+
+import bootstrap from 'bootstrap';
 
 function App() {
   return (
