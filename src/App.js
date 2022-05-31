f02264fb9a32769b0ec3df8514f78e273aaf6789
Eason <a28165965@gmail.com>
2022-05-31 18:55:55 +0800

    增加 stylesheet 資料夾

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
-// import 'bootstrap';
 import Navbar from './components/NavBar';
 import Footer from './components/Footer';
-
-import bootstrap from 'bootstrap';
 
 function App() {
   return (
