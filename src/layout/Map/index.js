5d5014921ecfb529e681645577b6a3b58aadcfdf
Zaynliao <103923760+Zaynliao@users.noreply.github.com>
2022-06-10 18:06:03 +0800

    [ADD]地區新增click hover效果(未完成)

--- src/layout/Map/index.js
+++ src/layout/Map/index.js
@@ -2,9 +2,25 @@ import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import MapData from './MapData.json';
 
-const center = [24, 121];
-
 function Index() {
+  const center = [24, 121];
+  const changeColor = (event) => {
+    event.target.setStyle({
+      color: 'black',
+      fillColor: 'black',
+    });
+  };
+  const onEachLocation = (location, layer) => {
+    layer.options.fillColor = location.properties.color;
+    const locationName = location.properties.COUNTYNAME;
+    console.log(locationName);
+    layer.bindPopup(`${locationName}`);
+
+    layer.on({
+      click: changeColor,
+    });
+  };
+
   return (
     <MapContainer
       center={center}
@@ -15,7 +31,18 @@ function Index() {
         url="https://api.maptiler.com/maps/hybrid/256/{z}/{x}/{y}.jpg?key=YdAyuapGGLNDoknjhGzG"
         attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
       />
-      <GeoJSON data={MapData} />
+      <GeoJSON
+        data={MapData.features}
+        style={() => ({
+          color: 'white',
+          weight: 2,
+          opacity: 0.7,
+          fillColor: 'white',
+          fillOpacity: 0.7,
+          dashArray: 3,
+        })}
+        onEachFeature={onEachLocation}
+      />
     </MapContainer>
   );
 }
