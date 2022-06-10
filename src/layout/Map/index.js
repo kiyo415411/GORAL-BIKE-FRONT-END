f0508be10e139ffedc323dea8af456cb858bdcfb
Zaynliao <103923760+Zaynliao@users.noreply.github.com>
2022-06-10 16:56:02 +0800

    [ADD]  完成地圖邊界設定

--- src/layout/Map/index.js
+++ src/layout/Map/index.js
@@ -1,7 +1,6 @@
-import { MapContainer, Polygon, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import MapData from './MapData.json';
-import { clearConfigCache } from 'prettier';
 
 const center = [24, 121];
 
@@ -16,29 +15,7 @@ function Index() {
         url="https://api.maptiler.com/maps/hybrid/256/{z}/{x}/{y}.jpg?key=YdAyuapGGLNDoknjhGzG"
         attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
       />
-
-      {MapData.features.map((state) => {
-        console.log(state.geometry.coordinates[0]);
-
-        const coordinates = state.geometry.coordinates[0].map((item) => [
-          item[1],
-          item[0],
-        ]);
-
-        return (
-          <Polygon
-            pathOptions={{
-              fillColor: '#000',
-              fillOpacity: 0.7,
-              weight: 2,
-              opacity: 1,
-              dashArray: 3,
-              color: 'white',
-            }}
-            positions={coordinates}
-          />
-        );
-      })}
+      <GeoJSON data={MapData} />
     </MapContainer>
   );
 }
