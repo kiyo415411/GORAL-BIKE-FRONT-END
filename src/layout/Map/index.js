b227b9764f33ada653473372207a918943df713d
Zaynliao <103923760+Zaynliao@users.noreply.github.com>
2022-06-11 13:13:03 +0800

    [ADD] 地圖各縣市區塊劃分 以及觸擊HOVER完成

--- src/layout/Map/index.js
+++ src/layout/Map/index.js
@@ -1,48 +1,71 @@
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import {
+  MapContainer,
+  TileLayer,
+  GeoJSON,
+  Tooltip,
+  Popup,
+} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import MapData from './MapData.json';
 
 function Index() {
   const center = [24, 121];
-  const changeColor = (event) => {
-    event.target.setStyle({
-      color: 'black',
-      fillColor: 'black',
-    });
-  };
-  const onEachLocation = (location, layer) => {
-    layer.options.fillColor = location.properties.color;
-    const locationName = location.properties.COUNTYNAME;
-    console.log(locationName);
-    layer.bindPopup(`${locationName}`);
-
-    layer.on({
-      click: changeColor,
-    });
-  };
 
   return (
     <MapContainer
       center={center}
       zoom={8}
-      style={{ width: '1920px', height: '960px' }}
+      style={{ width: '100%', height: '960px' }}
     >
       <TileLayer
         url="https://api.maptiler.com/maps/hybrid/256/{z}/{x}/{y}.jpg?key=YdAyuapGGLNDoknjhGzG"
         attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
       />
-      <GeoJSON
-        data={MapData.features}
-        style={() => ({
-          color: 'white',
-          weight: 2,
-          opacity: 0.7,
-          fillColor: 'white',
-          fillOpacity: 0.7,
-          dashArray: 3,
-        })}
-        onEachFeature={onEachLocation}
-      />
+      {MapData.features.map((value, index) => {
+        return (
+          <>
+            <GeoJSON
+              key={value.properties.COUNTYCODE}
+              data={value}
+              style={() => ({
+                color: 'white',
+                weight: 2,
+                opacity: 0.5,
+                fillColor: 'black',
+                fillOpacity: 0.5,
+                dashArray: 3,
+              })}
+              eventHandlers={{
+                mouseover: (e) => {
+                  const layer = e.target;
+                  layer.setStyle({
+                    color: 'black',
+                    weight: 2,
+                    opacity: 0.7,
+                    fillColor: 'white',
+                    fillOpacity: 0.5,
+                    dashArray: 3,
+                  });
+                },
+                mouseout: (e) => {
+                  const layer = e.target;
+                  layer.setStyle({
+                    color: 'white',
+                    weight: 2,
+                    opacity: 0.5,
+                    fillColor: 'black',
+                    fillOpacity: 0.5,
+                    dashArray: 3,
+                  });
+                },
+              }}
+            >
+              <Popup>{value.properties.COUNTYNAME}</Popup>
+              <Tooltip>{value.properties.COUNTYENG}</Tooltip>
+            </GeoJSON>
+          </>
+        );
+      })}
     </MapContainer>
   );
 }
