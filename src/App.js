1636253c4bf3d3509829a3684602934b2b326724
Zaynliao <103923760+Zaynliao@users.noreply.github.com>
2022-06-13 14:31:39 +0800

    [ADD]新增route nav link

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import Navbar from './layout/Public/NavBar';
 // import Index from './layout/Home';
 import Map from './layout/Map';
+import Index from './layout/Home/index';
 import Footer from './layout/Public/Footer';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
       <Map />
+      <Footer /> */}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/map" element={<Map />} />
+      </Routes>
       <Footer />
     </>
   );
