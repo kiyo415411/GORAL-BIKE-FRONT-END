e9c6efd99118da3f04957652a021edcd289fd1ce
Eason <a28165965@gmail.com>
2022-06-22 19:03:13 +0800

    [UPDATE] 登入功能連結加入

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
 import Navbar from './layout/Public/NavBar';
-// import Index from './layout/Home';
 import Map from './layout/Map';
 import Index from './layout/Home/index';
 import Footer from './layout/Public/Footer';
 import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
     <>
-      {/* <Navbar />
-      <Map />
-      <Footer /> */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/map" element={<Map />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
       </Routes>
       <Footer />
     </>
