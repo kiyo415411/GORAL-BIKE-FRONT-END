b9151c6a4bf919078930f50c0a303e377462cd11
Zaynliao <103923760+Zaynliao@users.noreply.github.com>
2022-06-25 23:38:21 +0800

    [ADD]首頁商品 CSS 滾動特效

--- src/layout/Home/index.js
+++ src/layout/Home/index.js
@@ -8,13 +8,24 @@ import DataAPI from '../Map/DataAPI';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import Slider from 'react-slick';
 
 //Slider
 import React from 'react';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import {
+  EffectCreative,
+  EffectCoverflow,
+  Mousewheel,
+  Navigation,
+} from 'swiper';
 
 export default function Index() {
   const [api, setApi] = useState([]);
@@ -46,11 +57,16 @@ export default function Index() {
     };
     getIndexData();
   }, []);
-  console.log(api);
-  console.log(news);
-  console.log(product);
-  console.log(activity);
-  console.log(course);
+
+  // 檢查生命週期區域
+  useEffect(() => {
+    // console.log(api);
+    // console.log(news);
+    // console.log(product);
+    // console.log(activity);
+    // console.log(course);
+    console.log(nav2);
+  }, [nav2]);
 
   return (
     <>
@@ -82,59 +98,91 @@ export default function Index() {
             </article>
           </section>
           {/* PRODUCT */}
-          <section className="my-5 py-5">
-            <Slider
-              asNavFor={nav2}
-              centerMode={true}
-              ref={(slider1) => setNav1(slider1)}
-              speed={1000}
+          <section className="w-75 mx-auto py-5">
+            <h1 className="w-60 mx-auto mt-4">熱門商品</h1>
+            <Swiper
+              navigation={true}
+              mousewheel={true}
+              modules={[EffectCoverflow, Navigation, Mousewheel]}
+              className="mySwiper"
+              loop={true}
+              // asNavFor={nav2}
+              // ref={(slider1) => setNav1(slider1)}
             >
               {product.map((value, index) => {
                 return (
-                  <section
-                    className="w-75 row d-flex justify-content-center align-items-center m-auto"
-                    style={{ height: '40rem' }}
-                  >
-                    <article className="col-4" style={{ textAlign: 'justify' }}>
-                      <h3>{value.product_name}</h3>
-                      <p className="mt-4 fs-6">
-                        鋁合金單避震登山車，採用較為直挺的騎乘幾何設定，Shimano
-                        Deore 1x10零組件搭配，Suntour避震前叉。
-                      </p>
-                      <p className="text-danger fw-bold text-end">查看更多</p>
-                    </article>
-                    <figure className="col-6 d-block ms-5 my-auto">
-                      <img
-                        className="img-fluid m-0 p-0 "
-                        style={{
-                          objectFit: 'contain',
-                          width: '50rem',
-                          height: '30rem',
-                        }}
-                        src={`${IMAGE_URL}/bikes/${value.product_images}`}
-                        alt=""
-                      />
-                    </figure>
-                  </section>
+                  <SwiperSlide key={index}>
+                    <section
+                      className="w-75 row d-flex justify-content-center align-items-center m-auto"
+                      style={{ height: '30rem' }}
+                    >
+                      <article
+                        className="col-5"
+                        style={{ textAlign: 'justify' }}
+                      >
+                        <h3>{value.product_name}</h3>
+                        <p className="mt-4 fs-6">
+                          鋁合金單避震登山車，採用較為直挺的騎乘幾何設定，Shimano
+                          Deore 1x10零組件搭配，Suntour避震前叉。
+                        </p>
+                        <p className="text-danger fw-bold text-end">查看更多</p>
+                      </article>
+                      <figure className="col-6 d-block ms-5 my-auto">
+                        <img
+                          className="img-fluid m-0 p-0"
+                          style={{
+                            objectFit: 'contain',
+                            width: '50rem',
+                            height: '30rem',
+                          }}
+                          src={`${IMAGE_URL}/bikes/${value.product_images}`}
+                          alt=""
+                        />
+                      </figure>
+                    </section>
+                  </SwiperSlide>
                 );
               })}
-            </Slider>
-            <Slider
-              asNavFor={nav1}
-              ref={(slider2) => setNav2(slider2)}
-              slidesToShow={5}
-              centerMode={true}
-              swipeToSlide={true}
-              focusOnSelect={true}
-              className="mt-5 w-75 mx-auto"
+            </Swiper>
+            <Swiper
+              effect={'coverflow'}
+              mousewheel={true}
+              slidesPerView={5}
+              grabCursor={true}
+              navigation={true}
+              centeredSlides={true}
+              // slidesPerView={'auto'}
+              coverflowEffect={{
+                rotate: 0,
+                stretch: 0,
+                depth: 0,
+                modifier: 1,
+                slideShadows: false,
+              }}
+              modules={[EffectCoverflow, Mousewheel, Navigation]}
+              loop={true}
+              className="mySwiper"
+
+              // asNavFor={nav1}
+              // ref={(slider2) => setNav2(slider2)}
             >
               {product.map((value, index) => {
-                return <h5 className="text-center">{value.product_name}</h5>;
+                return (
+                  <SwiperSlide key={index} style={{ height: '8rem' }}>
+                    <h6 className="text-center m-5 fs-5">
+                      {value.product_name}
+                    </h6>
+                  </SwiperSlide>
+                );
               })}
-            </Slider>
+            </Swiper>
           </section>
-          <section className="row p-5 overflow-hidden m-0">
-            <article className="col-4 my-auto me-5" style={{ width: '25%' }}>
+          {/* LOCATION */}
+          <section className="overflow-hidden">
+            <img src={LOCATION} alt="" />
+          </section>
+          <section className="row m-5 p-5 justify-content-center align-items-center">
+            <article className="col-2 my-auto me-5">
               <h2 className="fw-bold">登山車訓練營</h2>
               <p className="mt-3">
                 帶著這些問題，我們一起來審視登山車訓練營。需要考慮周詳登山車訓練營的影響及因應對策。如果此時我們選擇忽略登山車訓練營，那後果可想而知。當前最急迫的事，想必就是釐清疑惑了。這樣看來，對於登山車訓練營，我們不能不去想，卻也不能走火入魔。
@@ -143,40 +191,42 @@ export default function Index() {
                 更多訓練營
               </button>
             </article>
-            <section className="col-6">
-              {/* <Swiper
+            <section className="col-9 course">
+              <Swiper
                 effect={'coverflow'}
-                grabCursor={true}
-                centeredSlides={true}
-                slidesPerView={'auto'}
                 coverflowEffect={{
-                  rotate: 50,
+                  rotate: 30,
                   stretch: 0,
+                  scale: 0.5,
                   depth: 100,
                   modifier: 1,
                   slideShadows: true,
                 }}
-                pagination={true}
-                modules={[EffectCoverflow, Pagination]}
-                className="mySwiper h-50"
+                mousewheel={true}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={'auto'}
+                modules={[EffectCoverflow, Mousewheel]}
+                className="mySwiper my-auto h-100"
               >
                 {course.map((value, index) => {
                   return (
-                    <SwiperSlide>
+                    <SwiperSlide
+                      key={index}
+                      className="course_img_block my-auto"
+                    >
                       <img
+                        className="cover"
                         src={`${IMAGE_URL}/course/${value.course_pictures}`}
                         alt="..."
                       />
                     </SwiperSlide>
                   );
                 })}
-              </Swiper> */}
+              </Swiper>
             </section>
           </section>
-          {/* LOCATION */}
-          <section className="overflow-hidden">
-            <img src={LOCATION} alt="" />
-          </section>
+
           {/* ACTIVTY */}
           <section className="my-5 p-5 overflow-hidden">
             <h1 className="display-6 fw-bolder text-center">
