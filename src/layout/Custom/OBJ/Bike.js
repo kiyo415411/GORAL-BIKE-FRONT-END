b660085918b8f25e355606f625440c9230409db5
Zaynliao <103923760+Zaynliao@users.noreply.github.com>
2022-07-02 23:22:21 +0800

    [ADD]客製化物件

--- src/layout/Custom/OBJ/Bike.js
+++ src/layout/Custom/OBJ/Bike.js
@@ -1,24 +1,40 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useGLTF } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { proxy, useSnapshot } from 'valtio';
 
 export default function BikeModel({ ...props }) {
+  console.log({ ...props });
   const group = useRef();
-  const state = proxy({
-    current: null,
-    items: {
-      Wheels_MAT: '#006400',
-      Frame_MAT: '#00BFFF',
-      Saddle_MAT: '#FF00FF',
-      Metal_MAT: '#FF00FF',
-    },
-  });
-  const snap = useSnapshot(state);
+  const [hovered, set] = useState(null);
+  useEffect(() => {
+    const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`;
+    const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`;
+    if (hovered) {
+      document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(
+        cursor
+      )}'), auto`;
+      return () =>
+        (document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(
+          auto
+        )}'), auto`);
+    }
+  }, [hovered]);
+  const snap = useSnapshot(props.state);
   const { nodes, materials } = useGLTF('/bike.gltf');
-  useFrame((state, delta) => (group.current.rotation.y += 0.01));
+  // useFrame((state, delta) => (group.current.rotation.y += 0.005));
   return (
-    <group ref={group} {...props} dispose={null}>
+    <group
+      ref={group}
+      {...props}
+      dispose={null}
+      onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name))}
+      onPointerOut={(e) => e.intersections.length === 0 && set(null)}
+      onPointerMissed={() => (props.state.current = null)}
+      onClick={(e) => (
+        e.stopPropagation(), (props.state.current = e.object.material.name)
+      )}
+    >
       <group scale={0.02}>
         <mesh
           geometry={nodes.SeatPost.geometry}
@@ -28,6 +44,7 @@ export default function BikeModel({ ...props }) {
         <mesh
           geometry={nodes.SeatClampBottom.geometry}
           material={materials.Frame_MAT}
+          material-color={snap.items.Frame_MAT}
         />
         <mesh
           geometry={nodes.Saddle.geometry}
@@ -37,20 +54,28 @@ export default function BikeModel({ ...props }) {
           <mesh
             geometry={nodes.SaddleBarL.geometry}
             material={materials.Saddle_MAT}
+            material-color={snap.items.Saddle_MAT}
           />
           <mesh
             geometry={nodes.SaddleBarR.geometry}
             material={materials.Saddle_MAT}
+            material-color={snap.items.Saddle_MAT}
           />
         </mesh>
         <mesh
           geometry={nodes.SeatClampTop.geometry}
           material={materials.Frame_MAT}
+          material-color={snap.items.Frame_MAT}
         >
-          <mesh geometry={nodes.nut.geometry} material={materials.Metal_MAT} />
+          <mesh
+            geometry={nodes.nut.geometry}
+            material={materials.Metal_MAT}
+            material-color={snap.items.Metal_MAT}
+          />
           <mesh
             geometry={nodes.bolt_b_.geometry}
             material={materials.Metal_MAT}
+            material-color={snap.items.Metal_MAT}
           />
         </mesh>
         <mesh
@@ -58,138 +83,182 @@ export default function BikeModel({ ...props }) {
           material={materials.Metal_MAT}
           material-color={snap.items.Metal_MAT}
         />
-        <mesh geometry={nodes.stud.geometry} material={materials.Metal_MAT} />
+        <mesh
+          geometry={nodes.stud.geometry}
+          material={materials.Metal_MAT}
+          material-color={snap.items.Metal_MAT}
+        />
         <mesh
           geometry={nodes.Gear_p01.geometry}
           material={materials.DerailleurRear_MAT}
+          material-color={snap.items.DerailleurRear_MAT}
         />
         <mesh
           geometry={nodes.CrankGearLarge.geometry}
           material={materials.Crankset_MAT}
+          material-color={snap.items.Crankset_MAT}
         />
         <mesh
           geometry={nodes.CrankLeft.geometry}
           material={materials.Crankset_MAT}
+          material-color={snap.items.Crankset_MAT}
         >
           <mesh
             geometry={nodes['pedal_left-mesh'].geometry}
             material={materials.Pedal_MAT}
+            material-color={snap.items.Pedal_MAT}
           />
           <mesh
             geometry={nodes['pedal_left-mesh_1'].geometry}
             material={materials.Metal_MAT}
+            material-color={snap.items.Metal_MAT}
           />
         </mesh>
         <mesh
           geometry={nodes['DerailleurFront-mesh'].geometry}
           material={materials.DerailleurFront_MAT}
+          material-color={snap.items.DerailleurFront_MAT}
         />
         <mesh
           geometry={nodes['DerailleurFront-mesh_1'].geometry}
           material={materials.Metal_MAT}
+          material-color={snap.items.Metal_MAT}
         />
         <mesh
           geometry={nodes.CrankGearSmall.geometry}
           material={materials.Crankset_MAT}
+          material-color={snap.items.Crankset_MAT}
         />
         <mesh
           geometry={nodes.CrankRight.geometry}
           material={materials.Crankset_MAT}
+          material-color={snap.items.Crankset_MAT}
         >
           <mesh
             geometry={nodes['pedal_right-mesh'].geometry}
             material={materials.Pedal_MAT}
+            material-color={snap.items.Pedal_MAT}
           />
           <mesh
             geometry={nodes['pedal_right-mesh_1'].geometry}
             material={materials.Metal_MAT}
+            material-color={snap.items.Metal_MAT}
           />
         </mesh>
-        <mesh geometry={nodes.Chain.geometry} material={materials.Chain_MAT} />
+        <mesh
+          geometry={nodes.Chain.geometry}
+          material={materials.Chain_MAT}
+          material-color={snap.items.Chain_MAT}
+        />
         <mesh
           geometry={nodes['DerailleurRear-mesh'].geometry}
           material={materials.DerailleurRear_MAT}
+          material-color={snap.items.DerailleurRear_MAT}
         />
         <mesh
           geometry={nodes['DerailleurRear-mesh_1'].geometry}
           material={materials.Metal_MAT}
+          material-color={snap.items.Metal_MAT}
         />
         <mesh
           geometry={nodes.Gear_p02.geometry}
           material={materials.DerailleurRear_MAT}
+          material-color={snap.items.DerailleurRear_MAT}
         />
         <mesh
           geometry={nodes.CageBolt2.geometry}
           material={materials.Metal_MAT}
+          material-color={snap.items.Metal_MAT}
         />
         <mesh
           geometry={nodes.CageRear.geometry}
           material={materials.Cage_MAT}
+          material-color={snap.items.Cage_MAT}
         />
         <mesh
           geometry={nodes.CageBolt1.geometry}
           material={materials.Metal_MAT}
+          material-color={snap.items.Metal_MAT}
         />
         <mesh
           geometry={nodes.Bottle.geometry}
           material={materials.Bottle_MAT}
+          material-color={snap.items.Bottle_MAT}
         />
         <mesh
           geometry={nodes.CageFront.geometry}
           material={materials.Cage_MAT}
+          material-color={snap.items.Cage_MAT}
         />
         <mesh
           geometry={nodes.CageBolt3.geometry}
           material={materials.Metal_MAT}
+          material-color={snap.items.Metal_MAT}
         />
         <mesh
           geometry={nodes.CageBolt4.geometry}
           material={materials.Metal_MAT}
+          material-color={snap.items.Metal_MAT}
         />
-        <mesh geometry={nodes.Frame.geometry} material={materials.Frame_MAT}>
+        <mesh
+          geometry={nodes.Frame.geometry}
+          material={materials.Frame_MAT}
+          material-color={snap.items.Frame_MAT}
+        >
           <mesh
             geometry={nodes.pCylinder1.geometry}
             material={materials.Metal_MAT}
+            material-color={snap.items.Metal_MAT}
           />
           <mesh
             geometry={nodes['Brakes_Rear-mesh'].geometry}
             material={materials.Metal_MAT}
+            material-color={snap.items.Metal_MAT}
           />
           <mesh
             geometry={nodes['Brakes_Rear-mesh_1'].geometry}
             material={materials.Brakes_MAT}
+            material-color={snap.items.Brakes_MAT}
           />
           <mesh
             geometry={nodes.CableRearLower.geometry}
             material={materials.PaintBlack_MAT}
+            material-color={snap.items.PaintBlack_MAT}
           />
           <mesh
             geometry={nodes.pCylinder2.geometry}
             material={materials.Metal_MAT}
+            material-color={snap.items.Metal_MAT}
           />
           <mesh
             geometry={nodes.SkewerRearA.geometry}
             material={materials.PaintBlack_MAT}
+            material-color={snap.items.PaintBlack_MAT}
           />
           <mesh
             geometry={nodes.CableRearUpper.geometry}
             material={materials.PaintBlack_MAT}
+            material-color={snap.items.PaintBlack_MAT}
           />
           <mesh
             geometry={nodes.bolt_a_04.geometry}
             material={materials.Metal_MAT}
+            material-color={snap.items.Metal_MAT}
           />
           <mesh
             geometry={nodes.Cables_Front.geometry}
             material={materials.PaintBlack_MAT}
+            material-color={snap.items.PaintBlack_MAT}
           />
           <mesh
             geometry={nodes.SkewerRearB.geometry}
             material={materials.PaintBlack_MAT}
+            material-color={snap.items.PaintBlack_MAT}
           />
           <mesh
             geometry={nodes.pCylinder3.geometry}
             material={materials.Metal_MAT}
+            material-color={snap.items.Metal_MAT}
           />
         </mesh>
         <mesh
@@ -200,69 +269,89 @@ export default function BikeModel({ ...props }) {
         <mesh
           geometry={nodes['WheelFront_hiProfile-mesh_1'].geometry}
           material={materials.Metal_MAT}
+          material-color={snap.items.Metal_MAT}
         />
         <mesh
           geometry={nodes.magnet.geometry}
           material={materials.Computer_MAT}
+          material-color={snap.items.Computer_MAT}
         />
-        <mesh geometry={nodes.Fork.geometry} material={materials.Frame_MAT}>
+        <mesh
+          geometry={nodes.Fork.geometry}
+          material={materials.Frame_MAT}
+          material-color={snap.items.Frame_MAT}
+        >
           <mesh
             geometry={nodes.SkewerFrontA.geometry}
             material={materials.PaintBlack_MAT}
+            material-color={snap.items.PaintBlack_MAT}
           />
           <mesh
             geometry={nodes.ForkInnerMetal.geometry}
             material={materials.Metal_MAT}
+            material-color={snap.items.Metal_MAT}
           />
           <mesh
             geometry={nodes.SkewerFrontB.geometry}
             material={materials.PaintBlack_MAT}
+            material-color={snap.items.PaintBlack_MAT}
           />
           <mesh
             geometry={nodes.sensor.geometry}
             material={materials.Computer_MAT}
+            material-color={snap.items.Computer_MAT}
           />
           <mesh
             geometry={nodes['brakes_front-mesh'].geometry}
             material={materials.Metal_MAT}
+            material-color={snap.items.Metal_MAT}
           />
           <mesh
             geometry={nodes['brakes_front-mesh_1'].geometry}
             material={materials.Brakes_MAT}
+            material-color={snap.items.Brakes_MAT}
           />
         </mesh>
         <mesh
           geometry={nodes.HandleBars.geometry}
           material={materials.Frame_MAT}
+          material-color={snap.items.Frame_MAT}
         >
           <mesh
             geometry={nodes.HandleBarBolt2.geometry}
             material={materials.Metal_MAT}
+            material-color={snap.items.Metal_MAT}
           />
           <mesh
             geometry={nodes.tape.geometry}
             material={materials.HandlebarTape_MAT}
+            material-color={snap.items.HandlebarTape_MAT}
           />
           <mesh
             geometry={nodes.HandleBarBolt1.geometry}
             material={materials.Metal_MAT}
+            material-color={snap.items.Metal_MAT}
           />
           <mesh
             geometry={nodes['shifters-mesh'].geometry}
             material={materials.Shifters_MAT}
+            material-color={snap.items.Shifters_MAT}
           />
           <mesh
             geometry={nodes['shifters-mesh_1'].geometry}
             material={materials.Metal_MAT}
+            material-color={snap.items.Metal_MAT}
           />
         </mesh>
         <mesh
           geometry={nodes.WheelRear_hiProfile.geometry}
           material={materials.Wheels_MAT}
+          material-color={snap.items.Wheels_MAT}
         >
           <mesh
             geometry={nodes.CasetteGears.geometry}
             material={materials.Cassette_MAT}
+            material-color={snap.items.Cassette_MAT}
           />
         </mesh>
       </group>
--- src/layout/Custom/index.js
+++ src/layout/Custom/index.js
@@ -1,14 +1,70 @@
-import { Canvas, useFrame } from '@react-three/fiber';
-import React, { useRef, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import React, { useRef, useState, Suspense } from 'react';
 import BikeModel from './OBJ/Bike';
-import ManualBikeModel from './OBJ/Manual-bike';
+import { HexColorPicker } from 'react-colorful';
+import { proxy, useSnapshot } from 'valtio';
+import { OrbitControls } from '@react-three/drei';
+
+const state = proxy({
+  current: null,
+  items: {
+    Frame_MAT: '#eee',
+    Saddle_MAT: '#eee',
+    Metal_MAT: '#eee',
+    DerailleurRear_MAT: '#eee',
+    Crankset_MAT: '#eee',
+    Pedal_MAT: '#eee',
+    Chain_MAT: '#eee',
+    Cage_MAT: '#eee',
+    Bottle_MAT: '#eee',
+    Brakes_MAT: '#eee',
+    PaintBlack_MAT: '#eee',
+    Wheels_MAT: '#eee',
+    Computer_MAT: '#eee',
+    HandlebarTape_MAT: '#eee',
+    Shifters_MAT: '#eee',
+    Cassette_MAT: '#eee',
+  },
+});
+
+function Picker() {
+  const snap = useSnapshot(state);
+  return (
+    <div className="row w-40 me-auto m-5 justify-content-center align-items-center">
+      <HexColorPicker
+        className="picker col-6"
+        color={snap.items[snap.current]}
+        onChange={(color) => (state.items[snap.current] = color)}
+      />
+      <h1
+        className="col-6"
+        style={{
+          color: state.items[snap.current],
+        }}
+      >
+        {snap.current}
+      </h1>
+    </div>
+  );
+}
 
 export default function Custom() {
   return (
-    <Canvas className="vh-100">
-      <ambientLight />
-      <BikeModel />
-      <pointLight position={[10, 10, 10]} />
-    </Canvas>
+    <>
+      <nav>
+        <Picker />
+      </nav>
+      <main style={{ height: '600px' }}>
+        <Canvas camera={{ position: [0, 0, 4], fov: 50 }}>
+          <ambientLight />
+          <Suspense fallback={null}>
+            <BikeModel state={state} />
+          </Suspense>
+
+          <pointLight position={[10, 10, 10]} />
+          <OrbitControls makeDefault />
+        </Canvas>
+      </main>
+    </>
   );
 }
