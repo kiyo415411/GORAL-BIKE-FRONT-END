113757abc1f504a7c5e2f6c3935031816ff2ce7c
JamesVonThere <103923686+JamesVonThere@users.noreply.github.com>
2022-07-04 17:18:57 +0800

    [ADD]: Connect label to DB, connected check to DB

--- src/components/Label/LabelCard.js
+++ src/components/Label/LabelCard.js
@@ -1,8 +1,13 @@
+import { IMAGE_URL } from '../../utils/config';
+
 function LabelCard(props) {
   return (
     <div className="bg-line py-5 border border-white border-5 col-2 d-flex justify-content-center align-items-center flex-column">
-      <img src={require(`../../images/bikes/${props.name}.png`)} alt="bike" />
-      <p className="text-center m-1">{props.name}</p>
+      <img
+        src={`${IMAGE_URL}/bikes/parts/${props.partsIMG.replace(/ /g, '%20')}`}
+        alt="bike"
+      />
+      <p className="text-center m-1">{props.partsName}</p>
     </div>
   );
 }
