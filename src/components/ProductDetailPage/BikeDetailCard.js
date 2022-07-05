6d436756294ac7ff8a27ac6257a7bbee2b8eae50
JamesVonThere <103923686+JamesVonThere@users.noreply.github.com>
2022-07-05 09:50:41 +0800

    [FIX]: Detail page, switch color and img issue

--- src/components/ProductDetailPage/BikeDetailCard.js
+++ src/components/ProductDetailPage/BikeDetailCard.js
@@ -9,9 +9,9 @@ function BikeDetailCard(props) {
   const [partsIMG, setPartsIMG] = useState([]);
   const [bike, setBike] = useState([]);
   const [productCheck, setProductCheck] = useState([]);
-  const [BikeIMG] = useState('');
-  const [BikeStart] = useState([]);
-  const [bikeImages, setBikeImages] = useState('');
+  const [BikeIMG, setBikeIMG] = useState('');
+  const [BikeStart, setBikeStart] = useState([]);
+  const [bikeImage, setBikeImage] = useState('');
   const [currentColor, setCurrentColor] = useState('');
   const [downDesc, setDownDesc] = useState([]);
   useEffect(() => {
@@ -23,7 +23,16 @@ function BikeDetailCard(props) {
       });
 
       setBike(response.data.data);
-      setBikeImages(response.data.data[0].product_images);
+      setBikeImage(response.data.data[0].product_images);
+      setBikeIMG(response.data.data[0].product_images.split('.')[0]);
+      setBikeStart(
+        response.data.data[0].product_images.split('.')[
+          response.data.data[0].product_images.split('.').length - 1
+        ]
+      );
+      console.log(response.data.data[0].product_images.split('.'));
+      console.log('Bike STart', BikeStart);
+      console.log('setBIkeIMG', BikeIMG);
       setDownDesc(
         response.data.data[0].product_detail_description.split('&break')
       );
@@ -59,19 +68,6 @@ function BikeDetailCard(props) {
 
     // console.log('adfasdffa', productCheck);
   }, [props.product_id]);
-
-  const [bikeDetail] = useState([
-    {
-      Name: 'BIG NINE 15',
-      ShortDesc:
-        '腳踏車改變了我的命運。腳踏車，發生了會如何，不發生又會如何。如果此時我們選擇忽略腳踏車，那後果可想而知。',
-      LongDesc:
-        'ONE-TWENTY 600，FLOAT LINK浮動連桿避震平台，車體避震行程120mm，配置130mm避震前叉，Shimano優異的傳動系統，林道必備升降座桿，心之所向，無所不馭！ &break 在2019年推出的ONE-TWENTY車體概念後，市場大受歡迎，2020年的ONE-TWENTY 8000，甚至被Enduro Mountainbike雜誌評比為「BEST IN TEST」。到了2022，現代化的中行程林道車款，適用範圍依舊最廣大，在不失太多速度感、足夠輕量的設定之下，能盡情地享受越野騎乘的暢快體驗！美利達將其中再細分兩種屬性，一為更偏向XC越野繞圈賽的RC版本，後行程僅為100mm，搭配120mm前避震，頭管與立管角度稍大，更有利於爬坡；另一為最經典的120mm後避震行程，搭配130mm的避震前叉。組合出多款成車型號，任君挑選！',
-      Price: 308000,
-      Colors: ['#32CE13', '#E0CF05', '#D3484F', '#6F6669'],
-    },
-  ]);
-  const DownDesc = bikeDetail[0].LongDesc.split('&break');
   if (bike.length === 0) return <></>;
   return (
     <div className="container mt-5">
@@ -109,7 +105,7 @@ function BikeDetailCard(props) {
         <div className="row my-5 justify-content-between">
           {productCheck.map((v, i) => {
             return (
-              <h3 className="my-4 col-5">
+              <h3 className="my-4 col-5" key={i}>
                 <BsFillCheckCircleFill size={30} color="grey" /> {v}
               </h3>
             );
