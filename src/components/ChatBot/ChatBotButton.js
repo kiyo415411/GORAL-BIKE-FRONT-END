ab4b4e1e6e173bbf3fd322dd5b38ca74faeb4e96
kiyo415411 <barbie841111@gmail.com>
2022-07-11 12:13:32 +0800

    [ADD]加入導覽系統拖曳功能 + react-draggable 套件

--- src/components/ChatBot/ChatBotButton.js
+++ src/components/ChatBot/ChatBotButton.js
@@ -5,6 +5,9 @@ import Fab from '@mui/material/Fab';
 import { IMAGE_URL } from '../../utils/config';
 import { AiOutlineMinus } from 'react-icons/ai';
 
+import Draggable from 'react-draggable';
+import { useState } from 'react';
+
 export default function ChatBotButton({
   eventKey,
   callback,
@@ -38,34 +41,59 @@ export default function ChatBotButton({
   }
   const isCurrentEventKey = activeEventKey === eventKey;
 
+  const [state, setState] = useState({
+    activeDrags: 0,
+    deltaPosition: {
+      x: 0,
+      y: 0,
+    },
+    controlledPosition: {
+      x: -400,
+      y: 200,
+    },
+  });
+
+  const onStart = () => {
+    setState({ activeDrags: ++state.activeDrags });
+  };
+
+  const onStop = () => {
+    setState({ activeDrags: --state.activeDrags });
+  };
+
+  const dragHandlers = { onStart: onStart, onStop: onStop };
+  // const { deltaPosition, controlledPosition } = useState(state);
+
   return (
     <>
       {method === 'button' ? (
-        <div
-          className={
-            width < 768 && chatIn
-              ? 'd-none'
-              : 'position-fixed link-subcontent chat-bot-button'
-          }
-          style={{ zIndex: '98', bottom: '3rem', right: '3rem' }}
-          onClick={decoreatedAndConnect}
-        >
-          <Fab className="">
-            <div
-              className="rounded-circle overflow-hidden border "
-              style={{ width: '3.2rem', height: '3.2rem' }}
-            >
-              <img
-                className="cover"
-                src={`${IMAGE_URL}/avatar/Goral_Avatar.png`}
-                alt=""
-              />
-            </div>
-          </Fab>
-          <p className="mt-1 text-center" style={{ cursor: 'pointer' }}>
-            導覽羊
-          </p>
-        </div>
+        <Draggable bounds="body" {...dragHandlers}>
+          <div
+            className={
+              width < 768 && chatIn
+                ? 'd-none'
+                : 'position-fixed link-subcontent chat-bot-button'
+            }
+            style={{ zIndex: '98', bottom: '3rem', right: '3rem' }}
+            onClick={decoreatedAndConnect}
+          >
+            <Fab className="">
+              <div
+                className="rounded-circle overflow-hidden border"
+                style={{ width: '3.2rem', height: '3.2rem' }}
+              >
+                <img
+                  className="cover"
+                  src={`${IMAGE_URL}/avatar/Goral_Avatar.png`}
+                  alt=""
+                />
+              </div>
+            </Fab>
+            <p className="mt-1 text-center" style={{ cursor: 'pointer' }}>
+              導覽羊
+            </p>
+          </div>
+        </Draggable>
       ) : (
         <div
           className={`${useTheme.subText}  text-end me-3 pt-1`}
